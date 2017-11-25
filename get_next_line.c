c070c658998ea68a8d6f095b2bd5d98bed9ec6dc
Lucien LACAZE <llacaze@e1r12p11.42.fr>
2017-11-26 00:07:26 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: llacaze <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 14:53:45 by llacaze           #+#    #+#             */
-/*   Updated: 2017/11/25 16:00:56 by llacaze          ###   ########.fr       */
+/*   Updated: 2017/11/25 21:52:32 by llacaze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_check_eol(char *buf)
 	int		j;
 
 	j = 0;
-	write(1, "v", 1);
+	write(1, "u", 1);
 	while (buf[j] != EOL && buf[j])
 		j++;
 	if (buf[j] == EOL)
@@ -28,15 +28,17 @@ int		ft_check_eol(char *buf)
 	return (-1);
 }
 
-int		ft_check_buff(char **buf, char **stock, char **line)
+int		ft_check_buff(char **buf, char *stock, char **line)
 {
 	int		endline;
 
-	if (*buf == NULL)
-		*buf = ft_strdup(*stock);
-	*buf = ft_strjoin(*buf, *stock);
-	bzero(stock, BUFF_SIZE + 1);
+	//if (*buf == NULL)
+	//*buf = ft_memmove(*buf, stock, ft_strlen(stock));
+	*buf = ft_strdup(stock);
+	*buf = ft_strjoin(*buf, stock);
+	bzero(stock, BUFF_SIZE);
 	endline = ft_check_eol(*buf);
+	//printf("%d", endline);
 	if (endline > -1)
 	{
 		*line = ft_strdup(*buf);
@@ -50,17 +52,22 @@ int		get_next_line(const int fd, char **line)
 {
 	static char	*buf[217483648];
 	char		*stock;
-	int			reead;
+	int			check_buff;
+
 
 	stock = ft_memalloc(BUFF_SIZE + 1);
-	reead = read(fd, stock, BUFF_SIZE);
-	while (reead > 0)
+	while (read(fd, stock, BUFF_SIZE) > 0)
 	{
-		if (ft_check_buff(&buf[fd], &stock, line) == 1)
+		check_buff = ft_check_buff(&buf[fd], stock, line);
+		write(1, "v", 1);
+		free(stock);
+		if (check_buff == 1)
 			return (1);
-		reead = read(fd, stock, BUFF_SIZE);
+		stock = ft_memalloc(BUFF_SIZE + 1);
 	}
-	return (0);
+	if (check_buff == ft_check_buff(&buf[fd], stock, line))
+		return (0);
+	return (-1);
 }
 
 int		main(int ac, char **av)
@@ -70,11 +77,16 @@ int		main(int ac, char **av)
 	(void)ac;
 	fd = open(av[1], O_RDONLY);
 
-	write(1, "v", 1);
-	if (get_next_line(fd, &line) == 1)
+	write(1, "o", 1);
+	while (get_next_line(fd, &line) == 1)
 	{
 		ft_putendl(line);
 		free(line);
 	}
+	/*if (get_next_line(fd, &line) == 0)
+	{
+		ft_putendl(line);
+		free(line);
+	}*/
 	return (0);
 }
