6f2edcd385a461936b36637a5b33327837fac4f0
Lucien LACAZE <llacaze@e1r8p4.42.fr>
2017-11-29 16:45:15 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: llacaze <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 14:53:45 by llacaze           #+#    #+#             */
-/*   Updated: 2017/11/27 19:04:01 by llacaze          ###   ########.fr       */
+/*   Updated: 2017/11/29 11:56:33 by llacaze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int		ft_check_eol(char *buf)
 	int		j;
 
 	j = 0;
-//	write(1, "u", 1);
 	if (buf != NULL)
 	{
 		while (buf[j] != EOL && buf[j])
@@ -27,29 +26,26 @@ int		ft_check_eol(char *buf)
 			buf[j] = '\0';
 			return (j);
 		}
-//		if (buf[j] == '\0')
-//			return (0);
 	}
 	return (-1);
 }
 
-int		ft_check_buff(char **buf, char *stock, char **line)
+int		ft_check_buf(char **buf, char *stock, char **line)
 {
+	char	*tmp;
 	int		endline;
 
-
-	//if (buf == NULL)
-//	*buf = ft_strdup(stock);
-//	if (*buf != NULL)
+	tmp = *buf;
 	*buf = ft_strjoin(*buf, stock);
 	bzero(stock, BUFF_SIZE);
 	endline = ft_check_eol(*buf);
-	//printf("%d", endline);
+	free(tmp);
 	if (endline > -1)
 	{
 		*line = ft_strdup(*buf);
-//		printf("%s", *line);
+		tmp = *buf;
 		*buf = ft_strdup(*buf + endline + 1);
+		free(tmp);
 		return (1);
 	}
 	return (0);
@@ -59,65 +55,42 @@ int		get_next_line(const int fd, char **line)
 {
 	static char	*buf[217483648];
 	char		*stock;
-	int			check_buff;
+	size_t		check_buf;
+	int			ret;
 
 	stock = ft_memalloc(BUFF_SIZE + 1);
-	if (fd < 0)
+	if (fd < 0 || BUFF_SIZE <= 0 || !line || (ret = read(fd, stock, 0)) < 0)
 		return (-1);
-	while (read(fd, stock, BUFF_SIZE) > 0)
+	while ((ret = read(fd, stock, BUFF_SIZE)) > 0)
 	{
-		check_buff = ft_check_buff(&buf[fd], stock, line);
-		//write(1, "v", 1);
+		check_buf = ft_check_buf(&buf[fd], stock, line);
 		free(stock);
-		if (check_buff == 1)
+		if (check_buf == 1)
 			return (1);
-		stock = ft_memalloc(BUFF_SIZE + 1);
+		stock = ft_memalloc(BUFF_SIZE);
 	}
-	if (check_buff == ft_check_buff(&buf[fd], stock, line))
-		return (0);
-	if (check_buff == 0)
+	if (ft_check_buf(&buf[fd], stock, line) == 1)
+		return (1);
+	else if (ft_check_buf(&buf[fd], stock, line) == 0 && ft_strlen(buf[fd]) > 0)
 	{
-		write(1, "x", 1);
-		*line = ft_memalloc(ft_strlen(buf[fd]));
 		*line = ft_strdup(buf[fd]);
-		ft_strdel(buf);
+		ft_strdel(&buf[fd]);
+		free(*buf);
+		return (1);
 	}
-	/*else if (ft_strlen(buf[fd]) != 0)
-	{
-		write(1, "x", 1);
-		*line = ft_strdup(*buf);
-		free(buf);
-		free(stock);
-	}*/
-	printf("%d\n", check_buff);
 	return (0);
 }
-
+/*
 int		main(int ac, char **av)
 {
 	int		fd;
 	char	*line;
-	int			i = 0;
-	(void)ac;
-	fd = open(av[1], O_RDONLY);
 
-	while ((i = get_next_line(fd, &line)) > 0)
+	fd = open(av[1], O_RDONLY);
+	while (get_next_line(fd, &line) == 1)
 	{
-		printf("%d", i);
 		ft_putendl(line);
 		free(line);
-	//	i++;
 	}
-	//fd = open(av[2], O_RDONLY);
-	//while (get_next_line(fd, &line) == 1)
-	//{
-//		ft_putendl(line);
-//		free(line);
-//	}
-/*	if (get_next_line(fd, &line) == 0)
-	{
-		ft_putendl(line);
-		free(line);
-	}*/
 	return (0);
-}
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_getnextline.h                                   :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llacaze <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/23 14:24:25 by llacaze           #+#    #+#             */
-/*   Updated: 2017/11/27 14:30:10 by llacaze          ###   ########.fr       */
+/*   Created: 2017/11/29 10:25:08 by llacaze           #+#    #+#             */
+/*   Updated: 2017/11/29 11:24:24 by llacaze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_GETNEXTLINE_H
-# define FT_GETNEXTLINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <fcntl.h>
 # include <stdlib.h>
@@ -19,7 +19,7 @@
 # include <unistd.h>
 # include "libft/libft.h"
 
-# define BUFF_SIZE 4
+# define BUFF_SIZE 5606
 # define EOL '\n'
 
 int		get_next_line(const int fd, char **line);
